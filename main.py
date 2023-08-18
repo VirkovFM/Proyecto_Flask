f6cefe067fdb91278fc402595f513fa40a954305
VirkovFM <vfm72673@udelasalle.edu.mx>
2023-08-18 15:35:22 -0600

    proyectos agregado con minibug por if en main

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ app = Flask(__name__)
 def index():
     edad = 18
     #return edad, "<h1>VERDEEEEEEEEEEEEEEEEEEEEEEEE</h1>"
-    return render_template('index.html', edad)
+    return render_template('index.html', dato2 = ["1","2","3","4"])
 
 @app.route('/contacto')
 def contacto():
     total = 1000
     return "<h1>Bienvenidos a Flask - Contacto</h1>"
+
+@app.route('/proyectos')
+@app.route('/proyectos/<string:nombre>/<int:edad>')
+def proyectos(nombre = None, edad = 0):
+    if nombre is None:
+        return render_template('proyectos.html')
+    else:
+        return render_template('proyectos.html',edad = edad, nombre = nombre)
