e41d7a9d1164f82f4b2e6d7121754750eaceef9b
Fernando Miramontes <fernandomiramontes03@gmail.com>
2023-09-28 14:16:33 -0600

    Prueba

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, jsonify
+import MySQLdb
+from geopy.distance import great_circle
 
 app = Flask(__name__)
 
 # flask --app main.py run
-app = Flask(_name_)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://chemacruzp:123qweZXC@chemacruzp.mysql.pythonanywhere-services.com:3306/chemacruzp$default'
-db = SQLAlchemy(app)
 
-class Store(db.Model):
-    idStore = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(100))
-    lat = db.Column(db.String(20))
-    long = db.Column(db.String(20))
+# Conexion a la base de datos
 
+host = 'MrFerchoo.mysql.pythonanywhere-services.com'
+user = 'MrFerchoo'
+password = 'Jovencitos123'
+database = 'MrFerchoo$Store'
+
+connection = MySQLdb.connect(host=host, user=user, password=password, database=database)
+
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM store")
+data = cursor.fetchall()
+
+#   Rutas
 @app.route('/')   #esto sal escribiendo route y tab
 def index():
     edad = 18
@@ -31,11 +38,13 @@ def proyectos(nombre = None, edad = 0):
         return render_template('proyectos.html')
     else:
         return render_template('proyectos.html',edad = edad, nombre = nombre)
-    
+
+
 @app.route('/loops')
 def loops():
     lista = ["Frutas","Verdurass","Limpieza","Abarrotes"]
     return render_template('loops.html',lista = lista)  #El lista antes de lista es la variable del html
+
 
 @app.route('/mapa/<float:lat>/<float(signed=True):long>/<float(signed=True):zoom>/<float(signed=True):sizemap>/<float(signed=True):sizemaps>/<string:nombre>',methods=['GET'])
 def mapa(lat,long,zoom,sizemap,sizemaps,nombre):
@@ -51,31 +60,28 @@ def mapa(lat,long,zoom,sizemap,sizemaps,nombre):
    ]
     return render_template('mapa.html',lat=lat,long=long,markers=markers)
 
-@app.route('/nearest_stores', methods=['GET'])
+@app.route('/nearest', methods=['GET'])
 def nearest_stores():
-    try:
-        latitud_usuario = float(request.args.get('latitud'))
-        longitud_usuario = float(request.args.get('longitud'))
-    except ValueError:
-        return jsonify({"error": "Coordenadas inválidas"}), 400
+    # Obtén la latitud y longitud del usuario desde los parámetros de la solicitud
+    user_lat = float(request.args.get('lat'))
+    user_lon = float(request.args.get('lon'))
 
-    # Consulta las 5 ubicaciones más cercanas en la base de datos utilizando SQLAlchemy
-    ubicaciones_cercanas = (
-        db.session.query(Store)
-        .order_by(
-            ((Store.lat.cast(db.Float) - latitud_usuario) ** 2 + (Store.long.cast(db.Float) - longitud_usuario) ** 2)
-        )
-        .limit(5)
-        .all()
-    )
+    # Consulta las tiendas desde la base de datos
+    cursor = connection.cursor()
+    cursor.execute("SELECT name, lat, lon FROM store")
+    stores = cursor.fetchall()
 
-    # Convierte los resultados a un formato JSON
-    ubicaciones_json = [
-        {"name": store.name, "lat": store.lat, "long": store.long}
-        for store in ubicaciones_cercanas
-    ]
+    # Calcula las distancias entre el usuario y las tiendas
+    distances = []
+    for store in stores:
+        store_lat = store[1]
+        store_lon = store[2]
+        distance = great_circle((user_lat, user_lon), (store_lat, store_lon)).km
+        distances.append((store[0], distance))
 
-    return jsonify({"ubicaciones_cercanas": ubicaciones_json})
+    # Ordena las tiendas por distancia y selecciona las 5 más cercanas
+    nearest_stores = sorted(distances, key=lambda x: x[1])[:5]
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    # Devuelve las 5 tiendas más cercanas en formato JSON
+    result = [{'name': store[0], 'distance_km': store[1]} for store in nearest_stores]
+    return jsonify(result)
