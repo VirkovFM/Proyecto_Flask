95cbc1920797f633f19dce51520ea782a5df0dfd
Fernando Miramontes <fernandomiramontes03@gmail.com>
2023-09-29 12:09:35 -0600

    Prueba

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template, request, jsonify
-import MySQLdb
+from flask_sqlalchemy import SQLAlchemy
 from geopy.distance import great_circle
+import MySQLdb
 
 app = Flask(__name__)
 
-# flask --app main.py run
+# Configura la URI de la base de datos MySQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://MrFerchoo:Jovencitos123@MrFerchoo.mysql.pythonanywhere-services.com:3306/MrFerchoo$Store'
 
-# Conexion a la base de datos
+# Inicializa la extensión SQLAlchemy
+db = SQLAlchemy(app)
 
-host = 'MrFerchoo.mysql.pythonanywhere-services.com'
+# Define el modelo de datos Store
+class Store(db.Model):
+    idStore = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    lat = db.Column(db.String(20))
+    lon = db.Column(db.String(20))
+
+"""host = 'MrFerchoo.mysql.pythonanywhere-services.com'
 user = 'MrFerchoo'
 password = 'Jovencitos123'
 database = 'MrFerchoo$Store'
@@ -18,6 +28,7 @@ connection = MySQLdb.connect(host=host, user=user, password=password, database=d
 cursor = connection.cursor()
 cursor.execute("SELECT * FROM store")
 data = cursor.fetchall()
+"""
 
 #   Rutas
 @app.route('/')   #esto sal escribiendo route y tab
@@ -62,26 +73,34 @@ def mapa(lat,long,zoom,sizemap,sizemaps,nombre):
 
 @app.route('/nearest', methods=['GET'])
 def nearest_stores():
-    # Obtén la latitud y longitud del usuario desde los parámetros de la solicitud
-    user_lat = float(request.args.get('lat'))
-    user_lon = float(request.args.get('lon'))
+    try:
+        user_lat = float(request.args.get('lat'))
+        user_lon = float(request.args.get('lon'))
+        num_sucursales = int(request.args.get('num_sucursales', 5))
+    except ValueError:
+        return jsonify({"error": "Coordenadas inválidas o num_sucursales inválido"}), 400
 
     # Consulta las tiendas desde la base de datos
-    cursor = connection.cursor()
-    cursor.execute("SELECT name, lat, lon FROM store")
-    stores = cursor.fetchall()
+    stores = (
+        db.session.query(Store)
+        .with_entities(Store.name, Store.lat, Store.lon)  # Ajusté 'Store.long' a 'Store.lon'
+        .all()
+    )
 
     # Calcula las distancias entre el usuario y las tiendas
     distances = []
     for store in stores:
-        store_lat = store[1]
-        store_lon = store[2]
+        store_lat = float(store.lat)
+        store_lon = float(store.lon)  # Ajusté 'store.long' a 'store.lon'
         distance = great_circle((user_lat, user_lon), (store_lat, store_lon)).km
-        distances.append((store[0], distance))
+        distances.append((store.name, distance))
 
-    # Ordena las tiendas por distancia y selecciona las 5 más cercanas
-    nearest_stores = sorted(distances, key=lambda x: x[1])[:5]
+    # Ordena las tiendas por distancia y selecciona las 'num_sucursales' más cercanas
+    nearest_stores = sorted(distances, key=lambda x: x[1])[:num_sucursales]
 
-    # Devuelve las 5 tiendas más cercanas en formato JSON
+    # Devuelve las tiendas más cercanas en formato JSON
     result = [{'name': store[0], 'distance_km': store[1]} for store in nearest_stores]
     return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
